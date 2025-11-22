656c0641e6cee2de77a3e5593c08b9d62568beac
shaitansix <shaitansix3@gmail.com>
2025-11-22 11:11:06 -0500

    Configuracion de cors

--- src/config/cors.py
+++ src/config/cors.py
@@ -3,7 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_cors(app): 
   app.add_middleware(
     CORSMiddleware, 
-    allow_origins = ['*'], 
+    allow_origins = [
+      'http://localhost:5173', 
+      'http://localhost:5173/', 
+      'http://localhost:4173', 
+      'http://localhost:4173/',
+      'https://eda-laboratory-frontend.vercel.app', 
+      'https://eda-laboratory-frontend.vercel.app/'
+    ], 
     allow_credentials = True, 
     allow_methods = ['*'], 
     allow_headers = ['*']
